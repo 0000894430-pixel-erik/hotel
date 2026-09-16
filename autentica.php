79f746a8f60c590a1c332cd31e86aeec990d8182
Patricia <0000894430@senaimgaluno.com.br>
2026-09-15 21:55:24 -0300

    primeiro envio

---
+++ autentica.php
@@ -0,0 +1,21 @@
+<?php 
+require_once "conexao.php";
+
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$sql ="SELECT * FROM clientes WHERE email =
+'$email' and senha = '$senha'";
+
+$resultado = mysqli_query($conexao, $sql);
+
+if(mysqli_num_rows($resultado) > 0){
+    header("Location: minhas_reservas.php");
+}else{
+    header("location: login.html");
+    exit;
+
+}
+    
+
+?>
